6afb3cde40bce2fa557eef694ba091524def8b34
weak281 <164248072+weak281@users.noreply.github.com>
2024-05-23 18:22:51 +0300

    .

---
+++ Assets/AbdulazizAlaqel/c#/winer.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class winer : MonoBehaviour
+{
+    public void Crazy()
+   {
+        SceneManager.LoadScene(0);
+   }
+}
